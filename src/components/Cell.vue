401ee6d7e930ff5dab222f1e19be14b9503041b6
nick.gutsuliak <nick.gutsuliak@sunbeez.com>
2019-09-07 15:59:01 +0300

    Initial layout, styles

---
+++ src/components/Cell.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="cell">
+        <h3 class="cell-title">Cell #{{id}}</h3>
+        <form @submit.prevent="save">
+            <input type="number" class="input-field" v-model="cell.number" :disabled="cell.disabled">
+            <label>
+                <input type="checkbox" class="input-checkbox" v-model="cell.disabled">Disable input field
+            </label>
+            <button type="submit" class="submit-button" v-show="showButton">SAVE</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'Cell',
+        props: {
+            id: {
+                type: Number,
+                default: 0
+            }
+        },
+        data(){
+            return {
+                cell: {
+                    number: null,
+                    disabled: false
+                }
+            }
+        },
+        computed: {
+            showButton(){
+                return this.cell.number && !this.cell.disabled;
+            }
+        },
+        methods: {
+            save(){
+                console.log(this.cell);
+            }
+        },
+        created() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="main">
+        <div v-for="index in iterator">
+            <Cell :key="index" :id="index" />
+        </div>
+    </div>
+</template>
+
+<script>
+    import Cell from './Cell'
+
+    export default {
+        name: 'Main',
+        components: {
+            Cell
+        },
+        props: {},
+        data(){
+            return {
+                iterator: Array.from(Array(100).keys()),
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+
+        },
+        created() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
